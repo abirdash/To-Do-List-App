96a9b0b72627aa53de6b78f608206ef9c98af60b
abirdash <abirdash98@mail.com>
2025-07-14 17:20:37 +0600

    adding html, css, js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# To-Do-List-App
+# To-Do-List App
 A beautifully styled To-Do List built with pure HTML, CSS, and JavaScript. Add, mark, and delete tasks with persistent local storage — perfect for productivity lovers.
---
+++ script.js
@@ -0,0 +1,51 @@
+// const { createElement } = require("react");
+
+const search = document.getElementById('input');
+
+const btn = document.getElementById('btn');
+
+const result = document.getElementById('list-style');
+
+
+// for to add li and span into li
+function task () {
+    if(search.value ===''){
+        alert('Please enter something !')
+    }else{
+        let li = document.createElement('li'); //append li
+        li.innerHTML = search.value;
+        result.appendChild(li)
+        
+        let span = document.createElement('span'); // append span
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
+        
+        save(); // for the save data
+    }
+    search.value = '';
+}
+
+// for the button click
+btn.addEventListener('click',task);
+
+// for the result clik
+result.addEventListener('click', (e) =>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("ck");
+        save();
+    }else if(e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        save();
+    }
+})
+
+// for the save data
+function save () {
+    localStorage.setItem('data',result.innerHTML);
+}
+
+// for the get data form localStorage
+function get () {
+    result.innerHTML = localStorage.getItem('data');
+}
+get();
